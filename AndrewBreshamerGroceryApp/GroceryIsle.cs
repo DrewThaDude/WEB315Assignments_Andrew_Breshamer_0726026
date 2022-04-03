1c713d3dc8bd65038c8ced8758581d8dd7f597d5
studbury <W0726026@myscc.ca>
2022-04-03 18:57:02 -0400

    fix null exception
    set default List<> get;set; to stop exception from coming up

--- AndrewBreshamerGroceryApp/GroceryIsle.cs
+++ AndrewBreshamerGroceryApp/GroceryIsle.cs
@@ -7,5 +7,5 @@ public class GroceryIsle
     public string isleName { get; set; }
     public int isleNumber { get; set; }
 
-    public List<FoodItem> isleItems { get; set; }
+    public List<FoodItem> isleItems { get; set; } = new List<FoodItem>();
 }
