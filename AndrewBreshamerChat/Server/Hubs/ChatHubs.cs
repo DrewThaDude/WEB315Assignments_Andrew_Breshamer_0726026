8c9cc0e6ce823d42c36bc4c1bca709ef36eacfdf
studbury <W0726026@myscc.ca>
2022-04-15 16:30:13 -0400

    checkpoint

--- AndrewBreshamerChat/Server/Hubs/ChatHubs.cs
+++ AndrewBreshamerChat/Server/Hubs/ChatHubs.cs
@@ -1,13 +1,18 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
-namespace BlazorWebAssemblySignalRApp.Server.Hubs
+namespace AndrewBreshamerChat.Server.Hubs
 {
     public class ChatHub : Hub
     {
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+            //'Clients' Gets or sets an object that can be used to invoke methods on the clients connected to this hub.
+        }
+        public async Task SendMessageOthers(string user, string message)
+        {
+            await Clients.Others.SendAsync("SendMessage", user, "is typing •••");
         }
     }
 }
